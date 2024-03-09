4fe419947538b46739728bb109875e6be6c9c5df
Triobhuwan Pal <tribhuwanpal1729@gmail.com>
2024-03-09 16:32:16 +0530

    Project intialization

---
+++ Vehicle_Rental_System_Backend/src/main/java/Vehicle_Rental_System_Backend/VehicleRentalSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package Vehicle_Rental_System_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentalSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRentalSystemBackendApplication.class, args);
+	}
+
+}
---
+++ Vehicle_Rental_System_Backend/src/test/java/Vehicle_Rental_System_Backend/VehicleRentalSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Vehicle_Rental_System_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
